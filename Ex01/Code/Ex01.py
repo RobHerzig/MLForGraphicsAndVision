8c1adbbfecd7d8faab78b62e269ed8068855a82b
robexitus <robexitus91@web.de>
2018-05-01 16:06:15 +0200

    Ex 1_1 finished, probably

--- Ex01/Code/Ex01.py
+++ Ex01/Code/Ex01.py
@@ -9,15 +9,6 @@ import matplotlib.pyplot as plt
 from sklearn.neighbors import KDTree
 
 num_of_random_points = 2**10
-num_dimensions = 2
-
-def get_dim_vals():
-    dim_vals = []
-    i = 1
-    while i <= 491:
-        dim_vals.append(i)
-        i += 10
-    return dim_vals
 
 def find_nearest_neighbour_exhaustively(array, root_point):
     start_time = datetime.datetime.utcnow()
@@ -38,64 +29,74 @@ def find_nearest_neighbour_exhaustively(array, root_point):
     # print(str(len(root)) + "DIMS -> " + str(time_diff) + " || MIN = " + str(cur_min))
     return cur_min
 
-def test_dimensions(low, hi):
-    for dims in range(low, hi+1): #+1 because I want it to include the high value, pls don't judge
-        rands = np.random.rand(num_of_random_points, dims)
+def exhaustive_search_with_queries(points, queries):
+    start_time = datetime.datetime.utcnow()
+    for q in queries:
+        find_nearest_neighbour_exhaustively(points, q)
 
-        nearest_neighbour_of_first_point = find_nearest_neighbour_exhaustively(rands, 0)
-        print("NEAREST NEIGHBOUR OF " + str(rands[0]) + " IS " + str(nearest_neighbour_of_first_point))
+    end_time = datetime.datetime.utcnow()
+    time_diff = end_time - start_time
+    return time_diff.total_seconds() * 1000
 
-def benchmark_dimensions():
-    dim_vals = get_dim_vals()
-    print(dim_vals)
-    times = []
+def get_tree_from_array(array):
+    tree = KDTree(array, leaf_size=2)
+    return tree
 
-    # dim_vals = [1,2,3,4,5]
-    for i in dim_vals:
-        start_time = datetime.datetime.utcnow()
-        random_points = np.random.rand(num_of_random_points, i)
-        # print(random_points[0])
-        for point in random_points:
-            find_nearest_neighbour_exhaustively(random_points, point)
-        end_time = datetime.datetime.utcnow()
-        time_diff = end_time - start_time
-        times.append(time_diff.total_seconds() * 1000)
-        print(str(i) + " DIMENSIONS TOOK: " + str(time_diff))
+def kdtree_search(tree, queries):
+    start_time = datetime.datetime.utcnow()
+    for q in queries:
+        dist, ind = tree.query([q], k=3)
+        # print(str(dist) + " " + str(ind))
 
-    print(times)
-    return times
-    # plt.plot(dim_vals, times)
-    #
-    # axes = plt.gca()
-    # # axes.set_xlim([0,xmax])
-    # axes.set_ylim([0, 10000])
-    #
-    # plt.xlabel('Dimensions')
-    # plt.ylabel('Time in ms for exh. search')
-    # plt.title('Time for loop with diff. dims')
-    # plt.grid(True)
-    # plt.savefig("Ex01_1.png")
-    # plt.show()
-    #
+    end_time = datetime.datetime.utcnow()
+    time_diff = end_time - start_time
+    return time_diff.total_seconds() * 1000
 
-# def kdtree_search(tree, queries):
-#     # TODO
-#     pass
+test_no_kd = True
+test_kd = True
 
+times_kd = []
+times_no_kd = []
 
 if True:
     # timings for different D
-    times = benchmark_dimensions()
+    if test_no_kd:
+        for D in range(1, 500, 10):
+            random_points = np.random.rand(num_of_random_points, D)
+            time = exhaustive_search_with_queries(random_points, random_points) #just in case we want to use other points
+            times_no_kd.append(time)
+            print(str(time) + " FOR " + str(D) + " DIMENSIONS")
 
-    # # kd-tree search
-    # for D in range(1, 500, 10):
-    #     # TODO
-    #     pass
+        plt.plot(range(1, 500, 10), times_no_kd)
+        plt.title('Query Times without KD')
+        plt.xlabel('dimension (D)')
+        plt.ylabel('time (ms)')
+        plt.savefig('1_1_a_NO_KD_TREE.png', bbox_inches='tight')
 
-    # plot to file
-    plt.plot(range(1, 500, 10), times)
-    plt.title('Query Times')
-    plt.xlabel('dimension (D)')
-    plt.ylabel('time (ms)')
-    plt.savefig('1_1_c.png', bbox_inches='tight')
+    if test_kd:
+        # kd-tree search
+        for D in range(1, 500, 10):
+            print(D)
+            random_points = np.random.rand(num_of_random_points, D)
+            tree = get_tree_from_array(random_points)
+            time = kdtree_search(tree, random_points)
+            times_kd.append(time)
+            print(str(time) + " FOR " + str(D) + " DIMENSIONS")
+
+        # plot to file
+        plt.clf()
+        plt.plot(range(1, 500, 10), times_kd)
+        plt.title('Query Times')
+        plt.xlabel('dimension (D)')
+        plt.ylabel('time (ms)')
+        plt.savefig('1_1_c_ONLY_KD_TREE.png', bbox_inches='tight')
+
+    if test_kd and test_no_kd:
+        plt.clf()
+        plt.plot(range(1, 500, 10), times_no_kd)
+        plt.plot(range(1, 500, 10), times_kd)
+        plt.title('Query Times')
+        plt.xlabel('dimension (D)')
+        plt.ylabel('time (ms)')
+        plt.savefig('1_1_COMBINED.png', bbox_inches='tight')
 
